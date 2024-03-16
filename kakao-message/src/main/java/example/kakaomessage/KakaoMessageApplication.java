ecd0c6ed0fa60611b8f54c179756c1be3ad6a8cd
김현준 <khjlove8427@naver.com>
2024-03-16 13:51:21 +0900

    카카오 메시지 전달 예제 프로그램 레포지토리 생성

---
+++ kakao-message/src/main/java/example/kakaomessage/KakaoMessageApplication.java
@@ -0,0 +1,13 @@
+package example.kakaomessage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KakaoMessageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KakaoMessageApplication.class, args);
+	}
+
+}
---
+++ kakao-message/src/test/java/example/kakaomessage/KakaoMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package example.kakaomessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaoMessageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
